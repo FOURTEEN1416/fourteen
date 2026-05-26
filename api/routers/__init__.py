57094928b518b24298e0ebe1895e21786eb63743
FOURTEEN1416 <fourteen@users.noreply.github.com>
2026-05-26 18:56:56 +0800

    chore: 清理路由注册残留 + 更新子模块指针

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -1,13 +1,9 @@
 from api.routers.character_routes import router as character_router  # noqa: F401
 from api.routers.voice_routes import router as voice_router  # noqa: F401
-from api.routers.training_routes import router as training_router  # noqa: F401
-from api.routers.memory_routes import router as memory_router  # noqa: F401
 from api.routers.persona_card_routes import router as persona_card_router  # noqa: F401
 
 __all__ = [
     "character_router",
-    "training_router",
     "voice_router",
-    "memory_router",
     "persona_card_router",
 ]
